5149fca490a8e4a663783169cbd92c9dd19d5a5c
Antonella <adparra2@utpl.edu.ec>
2024-06-06 16:24:48 -0500

    Cambios

--- EjemploJava/HerenciaUno/src/herencia2/Ejecutor3.java
+++ EjemploJava/HerenciaUno/src/herencia2/Ejecutor3.java
@@ -8,44 +8,79 @@ package herencia2;
 import java.util.Locale;
 import java.util.Scanner;
 import herencia1.EstudianteDistancia;
+import herencia1.EstudiantePresencial;
 
 /**
  *
  * @author reroes
  */
 public class Ejecutor3 {
+
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
         // Ingresar por teclado
         // un objeto de tipo Estudiante Distancia si el usuario ingresa 1 como 
         // opción
         // un objeto de tipo Estudiante Presencial si el usuario ingresa 2 como 
         // opción
-        
-        Scanner entrada = new Scanner(System.in);
-        entrada.useLocale(Locale.US);
-        System.out.println("Ingrese nombres");
-        String nombres = entrada.nextLine();
-        System.out.println("Ingrese apellidos");
-        String apellidos = entrada.nextLine();
-        System.out.println("Ingrese identificación");
-        String identificacion = entrada.nextLine();
-        System.out.println("Ingrese edad");
-        int edad = entrada.nextInt();
-        System.out.println("Ingrese número de asignaturas");
-        int asignaturas = entrada.nextInt();
-        System.out.println("Ingrese costo asignatura");
-        double costoAsignatura = entrada.nextDouble();
-        EstudianteDistancia estudiante = new EstudianteDistancia();
-        
-        estudiante.establecerNombresEstudiante(nombres);
-        estudiante.establecerApellidoEstudiante(apellidos);
-        estudiante.establecerEdadEstudiante(edad);
-        estudiante.establecerIdentificacionEstudiante(identificacion);
-        estudiante.establecerNumeroAsginaturas(asignaturas);
-        estudiante.establecerCostoAsignatura(costoAsignatura);
-        estudiante.calcularMatriculaDistancia();
-        
-        System.out.printf("%s\n", estudiante);
-        
+        System.out.println("Estudiante Distancia             [1]");
+        System.out.println("Estudiante Presencial            [2]");
+        int op = entrada.nextInt();
+
+        entrada.nextLine();
+        switch (op) {
+            case 1: {
+                EstudianteDistancia estudiante = new EstudianteDistancia();
+                System.out.println("Ingrese nombres");
+                String nombres = entrada.nextLine();
+                System.out.println("Ingrese apellidos");
+                String apellidos = entrada.nextLine();
+                System.out.println("Ingrese identificación");
+                String identificacion = entrada.nextLine();
+                System.out.println("Ingrese edad");
+                int edad = entrada.nextInt();
+                System.out.println("Ingrese número de asignaturas");
+                int asignaturas = entrada.nextInt();
+                System.out.println("Ingrese costo asignatura");
+                double costoAsignatura = entrada.nextDouble();
+                estudiante.establecerNombresEstudiante(nombres);
+                estudiante.establecerApellidoEstudiante(apellidos);
+                estudiante.establecerEdadEstudiante(edad);
+                estudiante.establecerIdentificacionEstudiante(identificacion);
+                estudiante.establecerNumeroAsginaturas(asignaturas);
+                estudiante.establecerCostoAsignatura(costoAsignatura);
+                estudiante.calcularMatriculaDistancia();
+
+                System.out.printf("%s\n", estudiante);
+                break;
+            }
+            case 2: {
+                EstudiantePresencial estudiante2 = new EstudiantePresencial();
+                System.out.println("Ingrese nombres");
+                String nombres = entrada.nextLine();
+                System.out.println("Ingrese apellidos");
+                String apellidos = entrada.nextLine();
+                System.out.println("Ingrese identificación");
+                String identificacion = entrada.nextLine();
+                System.out.println("Ingrese edad");
+                int edad = entrada.nextInt();
+                System.out.println("Ingrese número de creditos");
+                int creditos = entrada.nextInt();
+                System.out.println("Ingrese costo creditos");
+                double costoCreditos = entrada.nextDouble();
+                estudiante2.establecerNombresEstudiante(nombres);
+                estudiante2.establecerApellidoEstudiante(apellidos);
+                estudiante2.establecerEdadEstudiante(edad);
+                estudiante2.establecerIdentificacionEstudiante(identificacion);
+                estudiante2.establecerNumeroCreditos(creditos);
+                estudiante2.establecerCostoCredito(costoCreditos);
+                estudiante2.calcularMatriculaPresencial();
+
+                System.out.printf("%s\n", estudiante2);
+                break;
+            }
+        }
+
     }
 }
